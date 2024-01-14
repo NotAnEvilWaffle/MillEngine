1545f3192dfdd825b919628f6e22d43945d06118
NotAnEvilWaffle <herobrinebudder149@gmail.com>
2024-01-14 14:20:32 -0600

    Revert to own Api solution instead of Gonkers

---
+++ MillEngine.ScryfallClient/Clients/ApiClient.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Caching.Memory;
+using MillEngine.ScryfallClient.Models;
+
+namespace MillEngine.ScryfallClient.Clients;
+
+public class ApiClient
+{
+    private IMemoryCache _cache;
+    private HttpClient _client;
+    private MemoryCacheEntryOptions _cacheOptions;
+    
+    public ApiClient(HttpClient client, IMemoryCache cache, string baseAddress, TimeSpan cacheTime)
+    {
+        _client = client;
+        _cache = cache;
+        _client.BaseAddress = new Uri(baseAddress);
+        _cacheOptions = new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = cacheTime };
+        
+    }
+
+    // Cache needs to be disabled in cases where getting a random card
+   public async Task<T> GetApiResponseAsync<T>(string endpoint, bool useCache = true) where T : BaseItem
+   {
+       string fullResourceUri = _client.BaseAddress.AbsoluteUri + endpoint;
+       if (_cache.TryGetValue(fullResourceUri, out T cachedResult))
+       {
+           return cachedResult;
+       }
+
+       try
+       {
+           HttpResponseMessage response = await _client.GetAsync(fullResourceUri);
+
+           if (response.IsSuccessStatusCode)
+           {
+               // Can't Deserialize asynchronously from a string for some reason
+               // So we need ReadAsStream instead of ReadAsString
+               var responseBody = await response.Content.ReadAsStreamAsync();
+               T? result = await JsonSerializer.DeserializeAsync<T>(responseBody);
+
+               _cache.Set(fullResourceUri, result, _cacheOptions);
+               return result;
+           }
+           
+           Console.WriteLine($"Error: {response.StatusCode} - {response.Content}");
+           return default(T);
+       }
+       catch (Exception e)
+       {
+           Console.WriteLine(e);
+           return default(T);
+       }
+   }
+}
---
+++ MillEngine.Shared/Models/Card.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Scryfall = ScryfallApi.Client.Models;
+
+namespace MillEngine.Shared.Models;
+
+public class Card
+{
+    public int CardId { get; set; }
+    public string CardName { get; set; }
+    public string TypeLine { get; set; }
+    public decimal Cmc { get; set; }
+    public string Power { get; set; }
+    public string Toughness { get; set; }
+    public string RawData { get; set; }
+    
+    [NotMapped]
+    public Scryfall.Card BaseCard { get; set; }
+}
