8f0958b0d2291c607fcefa86b92ee1abe5b516ea
NotAnEvilWaffle <herobrinebudder149@gmail.com>
2024-01-19 14:07:44 -0600

    Added Set client

--- MillEngine.ScryfallClient/Clients/ISetsClient.cs
+++ MillEngine.ScryfallClient/Clients/ISetsClient.cs
@@ -4,7 +4,9 @@ namespace MillEngine.ScryfallClient.Clients;
 
 public interface ISetsClient
 {
-   
-    
-    
+   Task<RequestedList<Set>> GetAllSets();
+
+   Task<Set> GetSetById(Guid id);
+
+
 }
--- MillEngine.ScryfallClient/Clients/SetsClient.cs
+++ MillEngine.ScryfallClient/Clients/SetsClient.cs
@@ -1,6 +1,25 @@
-﻿namespace MillEngine.ScryfallClient.Clients;
+﻿using MillEngine.ScryfallClient.Models;
+
+namespace MillEngine.ScryfallClient.Clients;
 
 public class SetsClient : ISetsClient
 {
+    private ApiClient _apiClient;
+
+    public SetsClient(ApiClient client)
+    {
+        _apiClient = client;
+    }
     
+    public Task<RequestedList<Set>> GetAllSets()
+    {
+        var setList = _apiClient.GetApiResponseAsync<RequestedList<Set>>("/sets");
+        return setList;
+    }
+
+    public Task<Set> GetSetById(Guid id)
+    {
+        var set = _apiClient.GetApiResponseAsync<Set>($"/sets/{id.ToString()}");
+        return set;
+    }
 }
