85f99de27f6fef32af54d9c8bf616681b475ddf0
NotAnEvilWaffle <herobrinebudder149@gmail.com>
2024-01-28 23:43:38 -0600

    Added LibraryDbContext.cs

---
+++ MillEngine.Server/LibraryDbContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MillEngine.Shared.Models.DTOs;
+
+namespace MillEngine.API;
+
+public class LibraryDbContext : DbContext
+{
+    public LibraryDbContext() { }
+    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+    
+    public virtual DbSet<DeckDTO> LibraryDecks { get; set; }
+    public virtual DbSet<CardDTO> LibraryCards { get; set; }
+    public virtual DbSet<CollectionDTO> LibraryCollections { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        
+    }
+
+    protected void OnLibraryDecksCreate(ModelBuilder builder)
+    {
+
+        builder.Entity<DeckDTO>()
+            .HasOne(x => x.Commander)
+            .WithOne()
+            .HasForeignKey<CardDTO>("DeckCommanderId")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<DeckDTO>()
+            .HasOne(x => x.Partner)
+            .WithOne()
+            .HasForeignKey<CardDTO>("DeckPartnerId")
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        
+        builder.Entity<DeckDTO>()
+            .HasMany(x => x.DeckCards)
+            .WithOne()
+            .HasForeignKey("DeckMainId")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<DeckDTO>()
+            .HasMany(x => x.SideBoard)
+            .WithOne()
+            .HasForeignKey("DeckSideId")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<DeckDTO>()
+            .HasMany(x => x.MaybeList)
+            .WithOne()
+            .HasForeignKey("DeckMaybeId")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<DeckDTO>()
+            .HasMany(x => x.WishList)
+            .WithOne()
+            .HasForeignKey("DeckWishId")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+    protected void OnLibraryCollectionsCreate(ModelBuilder builder)
+    {
+        builder.Entity<CollectionDTO>()
+            .HasMany(x => x.CollectionCards)
+            .WithOne()
+            .HasForeignKey("CollectionId")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
