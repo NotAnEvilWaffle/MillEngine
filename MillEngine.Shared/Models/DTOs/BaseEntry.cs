ac01af2de8e6674d2200cc966da4081d45a4ee07
NotAnEvilWaffle <herobrinebudder149@gmail.com>
2024-02-05 14:07:51 -0600

    Fixed Blazor Form Validation

--- MillEngine.Shared/Models/DTOs/BaseEntry.cs
+++ MillEngine.Shared/Models/DTOs/BaseEntry.cs
@@ -6,5 +6,6 @@ public abstract class BaseEntry
 {
     [Key]
     public int Id { get; init; }
-    public string Name { get; init; }
+    [Required]
+    public string Name { get; set; }
 }
--- MillEngine.Shared/Models/DTOs/DeckEntry.cs
+++ MillEngine.Shared/Models/DTOs/DeckEntry.cs
@@ -7,7 +7,9 @@ public class DeckEntry : BaseEntry
     public List<CardEntry> MaybeList { get; init; } = [];
     public List<CardEntry> WishList { get; init; } = [];
     
-    public CardEntry Commander { get; set; }
-    public CardEntry Partner { get; set; }
+    // A deck does not require a Commander nor a Partner
+    // Also nullable because JSON will not set it by default
+    public CardEntry? Commander { get; set; }
+    public CardEntry? Partner { get; set; }
     
 }
