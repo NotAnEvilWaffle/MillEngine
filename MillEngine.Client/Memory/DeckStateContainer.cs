e7e6797932bb2c8b6eef813ae9472035e3d970f8
NotAnEvilWaffle <herobrinebudder149@gmail.com>
2024-02-09 23:10:08 -0600

    Added DeckStateContainer.cs to avoid extra db queries

---
+++ MillEngine.Client/Memory/DeckStateContainer.cs
@@ -0,0 +1,21 @@
+﻿using MillEngine.Shared.Models.DTOs;
+
+namespace MillEngine.Client.Memory;
+
+public class DeckStateContainer
+{
+    private DeckEntry? savedDeck;
+
+    public DeckEntry? SavedDeck
+    {
+        get => savedDeck;
+        set
+        {
+            savedDeck = value;
+            NotifyOnStateChanged();
+        }
+    }
+
+    public event Action? OnChange;
+    private void NotifyOnStateChanged() => OnChange?.Invoke();
+}
